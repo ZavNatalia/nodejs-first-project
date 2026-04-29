78df860fecebf9ffdd8e46bc0e51ad15987e9615
NZ <ns@zav.me>
2026-04-29 17:19:29 +0300

    add express

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes.handler);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>404 Not Found!</h1>');
+})
+
+app.listen(3000);
