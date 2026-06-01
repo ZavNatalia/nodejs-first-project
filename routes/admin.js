832cb595897825ee2f3a2516603ead5ceccccae6
NZ <ns@zav.me>
2026-06-01 15:12:44 +0300

    add pug

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,17 @@ const path = require('path');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res)=>{
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html')); //  html
+    res.render('add-product', {pageTitle: 'Add Product'});
 });
 
 router.post('/add-product', (req, res, next)=> {
+    products.push({title: req.body.title, price: req.body.price});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,13 @@ const path = require('path');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res)=>{
-    res.sendFile(path.join(rootDir,'views', 'shop.html'));
+    // res.sendFile(path.join(rootDir,'views', 'shop.html')); //html
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'});
+
 });
 
 module.exports = router;
