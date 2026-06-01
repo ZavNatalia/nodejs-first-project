9ae6cf71a2ee78f50382d286a181d9f9de43e85a
NZ <ns@zav.me>
2026-06-01 16:04:47 +0300

    add main-layout.pug

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const adminData = require('./admin');
 router.get('/', (req, res)=>{
     // res.sendFile(path.join(rootDir,'views', 'shop.html')); //html
     const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop'});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 
 });
 
